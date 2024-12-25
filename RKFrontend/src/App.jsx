56180b5daed8ff562f10d7054fdecfed3f58eed1
franfreezy <frandelwanjawa@yahoo.com>
2024-12-25 17:12:52 +0300

    push

--- RKFrontend/src/App.jsx
+++ RKFrontend/src/App.jsx
@@ -1,26 +1,28 @@
 
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './js/scripts.js';
+import "./js/material.js";
 import ProtectedRoute from './pages/@protect/ProtectedRoute';
 import Login from "./auth/login";
 import Register from "./auth/register";
 import Dashboard from "./pages/dashboard";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 function App() {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<Dashboard />} />
       <Route path="/register" element={<Register />} />
 
       {/* Protect these routes */}
       <Route
         path="/dashboard"
-        element={<ProtectedRoute element={<Dashboard />} />}
+        element={<ProtectedRoute element={<Login />} />}
       />
       
     </Routes>
