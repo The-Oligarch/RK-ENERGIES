573ca888570bac81f3fdcbd3c6486e6431f8df5e
ReyKiptoo <kiptoorey@gmail.com>
2025-01-26 12:20:35 +0300

    update

--- RKEBackend/backendapi/views.py
+++ RKEBackend/backendapi/views.py
@@ -21,9 +21,7 @@ from django.shortcuts import render, redirect
 from backend.models import CustomUser
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated,AllowAny
-from rest_framework.response import Response
 from rest_framework import status
-import json
 from rest_framework.views import APIView
 from backend.models import espPayload
 from django.views.decorators.csrf import csrf_exempt 
@@ -127,53 +125,9 @@ import json
 
 @permission_classes([AllowAny])
 class espPayloadHandling(APIView):
-    def post(self, request, *args, **kwargs):
-        try:
-            if isinstance(request.data, dict) and '_content' not in request.data:
-                data = request.data
-                print("Parsed as JSON:", data)
-            else:
-                data = dict(request.data)
-                print("QueryDict Data:", data)
-                
-                data_json = data.get('_content', '')  
-                print(data_json)
-                data_json = data_json[0].replace("\r\n", "")  
-                data = json.loads(data_json) 
-                print("Extracted Data:", data)
-            
-            phone = data.get('phone')
-            amount = data.get('amount')
-            fuel = data.get('fuel')
-            fuelstation = data.get('fuelstation')
-
-            if None in [phone, amount, fuel, fuelstation]:
-                return Response({"error": "Missing required fields"}, status=status.HTTP_400_BAD_REQUEST)
-
-            payload = espPayload.objects.create(
-                phone=phone,
-                amount=amount,
-                fuel=fuel,
-                fuelstation=fuelstation
-            )
-
-            return Response(
-                {
-                    "message": "espPayload saved successfully",
-                    "payload_id": payload.id,
-                    "fuel": payload.fuel,
-                    "amount": payload.amount,
-                    "phone": payload.phone,
-                    "fuelstation": payload.fuelstation
-                
-                },
-                status=status.HTTP_201_CREATED,
-            )
-
-        except json.JSONDecodeError:
-            return Response({"error": "Invalid JSON format"}, status=status.HTTP_400_BAD_REQUEST)
-        except Exception as e:
-            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    def get(self, request, *args, **kwargs):
+        # Handle GET request here
+        return JsonResponse({"message": "Success"})
 
     def get(self, request, *args, **kwargs):
         try:
